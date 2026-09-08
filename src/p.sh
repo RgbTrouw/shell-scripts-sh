e8cb7d24e7a399da8839104d594533ef4890fa6d
Rgb Trouw / Rgb Trust <40927728+RgbTrouw@users.noreply.github.com>
2026-09-08 11:05:17 +0300

    Create p.sh for detached process lock management
    Add a script for managing a detached process lock.

---
+++ src/p.sh
@@ -0,0 +1,22 @@
+# © RgbTrust 2026
+# detached process lock (management)
+# ./p.sh
+
+echo "1" > $0.lock;
+while true;
+lock=$(head -c 1 $0.lock;
+
+if(($lock == 1));
+then
+
+echo "working...";
+sleep 1;
+clear;
+
+else 
+exit 1;
+
+fi
+done&
+
+# echo "0" > p.sh.lock;
